fd696d516ca06f42d053a02e5af1b640f96ca02f
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-04-26 10:55:26 +0300

    Pow of Two Practice

---
+++ src/Week1/PowOfTwo.java
@@ -0,0 +1,26 @@
+package Week1;
+
+import java.util.Scanner;
+
+public class PowOfTwo {
+
+    public static void main(String[] args) {
+
+        int userInput;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the num: ");
+        userInput = input.nextInt();
+
+        for (int i=0;i<=userInput;i++) {
+            if (Math.pow(4,i)<=userInput || Math.pow(5,i)<=userInput) {
+                System.out.printf("4^%s<=%s\n",i,userInput);
+                System.out.printf("5^%s<=%s\n",i,userInput);
+            }
+        }
+
+
+    }
+
+}
