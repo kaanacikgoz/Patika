ebc3ba0c0d2611da0e45fbf34b4cb16a45d87660
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-04-29 01:33:16 +0300

    Fixed bug in Pow Of Two Practice

--- src/Week1/PowOfTwo.java
+++ src/Week1/PowOfTwo.java
@@ -14,13 +14,12 @@ public class PowOfTwo {
         userInput = input.nextInt();
 
         for (int i=0;i<=userInput;i++) {
-            if (Math.pow(4,i)<=userInput || Math.pow(5,i)<=userInput) {
+            if (Math.pow(4,i)<=userInput)  {
                 System.out.printf("4^%s<=%s\n",i,userInput);
+            }
+            if (Math.pow(5,i)<=userInput) {
                 System.out.printf("5^%s<=%s\n",i,userInput);
             }
         }
-
-
     }
-
 }
