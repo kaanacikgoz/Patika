4fba01011fe9e6215161a8175e0f1f307d19dda9
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-05-01 03:24:03 +0300

    Updated Prime Numbers Homework

--- src/Week2/PrimeNumbers.java
+++ src/Week2/PrimeNumbers.java
@@ -3,24 +3,32 @@ package Week2;
 import java.util.Scanner;
 
 public class PrimeNumbers {
-
+    //While dongusune girsin diye 2'ye eşitledim.
     static int userInput=2;
+
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
 
+        /* Bir inputtan sonra dongu devam etsin diye while kullandim.
+        -1,0,1'de hata verdigi icin try-catch blogu ile daha guzel bir
+        cikti elde ettim. */
         while (userInput > 1) {
             System.out.print("Enter the num: ");
             userInput = input.nextInt();
-
-            if (isPrimeNumber(userInput, userInput / 2)) {
-                System.out.println(userInput + " is a prime number.");
-            } else {
-                System.out.println(userInput + " is not a prime number.");
+            try {
+                if (isPrimeNumber(userInput, userInput / 2)) {
+                    System.out.println(userInput + " is a prime number.");
+                } else {
+                    System.out.println(userInput + " is not a prime number.");
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
         }
     }
 
+    //Recursive method ile girilen sayinin asal olup olmadıgını bulma
     public static boolean isPrimeNumber(int number, int divisor) {
         if (divisor==1) {
             return true;
