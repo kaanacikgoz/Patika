03093cf55da3ecb6a3cc48ee70d698aa1e3fc74c
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-05-06 16:16:11 +0300

    Added printed right answer and printed wrong choice array

--- src/Week2/GuessNumber.java
+++ src/Week2/GuessNumber.java
@@ -1,5 +1,6 @@
 package Week2;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -14,19 +15,24 @@ public class GuessNumber {
     }
 
     static void guessNumber() {
-        int right=6, randomNum = random.nextInt(100);
+        int right=6, index=0, randomNum = random.nextInt(100);
+        int[] wrongArray = new int[right];
+        boolean isWin=false;
         while (0<right) {
             System.out.print("Enter your guess: ");
             int guess = input.nextInt();
             if (guess < 100 && guess >= 0) {
                 if (guess == randomNum) {
                     System.out.println("Congrats! The number is " + randomNum);
+                    isWin = true;
                     break;
                 } else {
                     right--;
                     System.out.printf("Left %s live\n", right);
+                    wrongArray[index] = guess;
+                    index++;
                     if (right==0) {
-                        System.out.print("You Lost!");
+                        System.out.println("You Lost!");
                         break;
                     }
                     if (guess>randomNum) {
@@ -39,6 +45,10 @@ public class GuessNumber {
                 System.out.println("Please enter between 0-100 num!");
             }
         }
+        if (!isWin) {
+            System.out.println("Right answer is: "+randomNum);
+        }
+        System.out.print("Wrong choices: "+Arrays.toString(wrongArray));
     }
 
 }
